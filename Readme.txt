388dd709e949387f61c3af029d1f23ef8574eb44
Nilesh kumar <nileshmahala05@gmail.com>
2017-12-09 22:31:01 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+1. In config.h set your AIOkey and your wifi name and password
+2 create an account on adafruit.io so you will get user name and password
+3. create  a new feed name will be given in the code
+4 here we are using two feeds we are controlling two leds using google assistant
+5 create a new dashboard for two feeds
+6. we are also control the brightness of the leds
+7. we have to connect the adafruit.io account to the IFTTT(go to home and connceted)
+8. in IFTTT we create new applet IF THIS THEN THAT
+9. first we register to the email that is useful when we control the led by google assistant
+10. make applets for controlling the led 
---
+++ Voicecontroltwoled.ino
@@ -0,0 +1,133 @@
+// Adafruit IO Digital Output Example
+// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-digital-output
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+
+/************************ Example Starts Here *******************************/
+
+// digital pin 5
+#define LED_PIN1 5
+#define LED_PIN2 4
+
+// set up the 'digital' feed
+AdafruitIO_Feed *digital1 = io.feed("digital1");
+AdafruitIO_Feed *digital2 = io.feed("digital2");
+AdafruitIO_Feed *analog1 = io.feed("analog1");
+AdafruitIO_Feed *analog2 = io.feed("analog2");
+
+
+
+void setup() {
+
+  // set led pin as a digital output
+  pinMode(LED_PIN1, OUTPUT);
+   pinMode(LED_PIN2, OUTPUT);
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // set up a message handler for the 'digital' feed.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+  digital1->onMessage(handleMessage1);
+   digital2->onMessage(handleMessage2);
+   analog1->onMessage(handleMessage3);
+analog2->onMessage(handleMessage4);
+
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+}
+
+// this function is called whenever an 'digital' feed message
+// is received from Adafruit IO. it was attached to
+// the 'digital' feed in the setup() function above.
+void handleMessage1(AdafruitIO_Data *data) {
+
+  Serial.print("received <- ");
+
+  if(data->toPinLevel() == HIGH)
+    Serial.println("HIGH");
+  else
+    Serial.println("LOW");
+
+  // write the current state to the led
+  digitalWrite(LED_PIN1, data->toPinLevel());
+  
+
+}
+void handleMessage2(AdafruitIO_Data *data) {
+
+  Serial.print("received <- ");
+
+  if(data->toPinLevel() == HIGH)
+    Serial.println("HIGH");
+  else
+    Serial.println("LOW");
+
+  // write the current state to the led
+  
+  digitalWrite(LED_PIN2, data->toPinLevel());
+
+}
+void handleMessage3(AdafruitIO_Data *data) {
+
+  // convert the data to integer
+  int reading = data->toInt();
+
+  Serial.print("received <- ");
+  Serial.println(reading);
+  analogWrite(LED_PIN1, reading);
+
+}
+void handleMessage4(AdafruitIO_Data *data) {
+
+  // convert the data to integer
+  int reading = data->toInt();
+
+  Serial.print("received <- ");
+  Serial.println(reading);
+  analogWrite(LED_PIN2, reading);
+
+}
